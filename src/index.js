29e101caed0316992008de414a7f1fdd917b6281
Habenzy <robertrstauss@gmail.com>
2018-11-12 15:31:29 -0500

    added buttons (directional broken, no scoring yet) answer only displays on guess submit

--- src/index.js
+++ src/index.js
@@ -19,11 +19,9 @@ function randomLong() {
 }
 function isInVT(lat, long) {
   let result = leafletPip.pointInLayer([long, lat], vermont);
-  console.log("result: " + result)
-  console.log("length of VT array: " + result.length);
   if (result.length >= 1) {
     return true;
-  } 
+  }
   return false;
 }
 function getNewPoint() {
@@ -42,7 +40,7 @@ class GameMap extends React.Component {
   constructor(props) {
     super(props)
     //this.mapRef = React.createRef();
-    this.map = null;   
+    this.map = null;
     this.state = {
       currentCoords: [44.4759, -73.2121]
     }
@@ -59,20 +57,50 @@ class GameMap extends React.Component {
     this.map = null;
   };
 
-  goToMysteryLocation(){
-    console.log(this.props.answerCoords)        
+  goToMysteryLocation() {
+    console.log(this.props.answerCoords)
     this.map.panTo(this.props.answerCoords);
-    this.setState({currentCoords: this.props.answerCoords})
-    this.showAnswer();
+    this.setState({ currentCoords: this.props.answerCoords })
   };
 
-  showAnswer() {
-    let answermarker = new Leaflet.marker(this.props.answerCoords, { draggable: false }).addTo(this.map);
-    answermarker.bindPopup("<strong>Actual Location</strong>").addTo(this.map).openPopup();
-  };
+  moveMap = (verticalOffset, horizontalOffset) => {
+    this.currentCoords = this.map.getCenter();
+    console.log("current Center: " + this.currentCoords);
+    let currentLat = this.currentCoords.lat;
+    let currentLng = this.currentCoords.lng;
+    console.log("current Lat: " + currentLat);
+    console.log("current Lng: " + currentLng);
+    this.map.panTo((currentLat + verticalOffset), (currentLng + horizontalOffset));
+    this.currentCoords = this.map.getCenter();
+    console.log("new center: " + this.currentCoords)
+  }
+
+  moveNorth = () => {
+    this.moveMap(.002, 0)
+  }
+
+  moveEast = () => {
+    this.moveMap(0, .003)
+  }
+
+  moveWest = () => {
+    this.moveMap(0, -.003)
+  }
+
+  moveSouth = () => {
+    this.moveMap(-.002, 0)
+  }
 
   render() {
-    return <div ref={this.mapRef} id="game-map" className="map"/>;
+    return (
+      <div id="game-area">
+        <button onClick={this.moveNorth}>NORTH</button>
+        <button onClick={this.moveEast}>EAST</button>
+        <div ref={this.mapRef} id="game-map" className="map" />
+        <button onClick={this.moveSouth}>SOUTH</button>
+        <button onClick={this.moveWest}>WEST</button>
+      </div>
+    )
   }
 }
 
@@ -85,14 +113,14 @@ class GuessMap extends React.Component {
   }
   componentDidMount() {
     console.log("guess coordinates : " + this.props.guessCoords)
-    this.map = Leaflet.map('guess-map').setView([44., -72.7317], 7.5);
+    this.map = Leaflet.map('guess-map').setView([44, -72.7317], 7.5);
     Leaflet.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
       maxZoom: 19,
       attribution: 'Imagery from <a href="http://giscience.uni-hd.de/">GIScience Research Group @ University of Heidelberg</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(this.map);
     vermont.addTo(this.map);
     this.map.once('click', this.addMarker);
-    this.showAnswer();
+    //this.showAnswer();
   }
 
   componentWillUnmount() {
@@ -101,30 +129,40 @@ class GuessMap extends React.Component {
   }
 
   addMarker = (e) => {
-    const {lat, lng} = e.latlng;
+    const { lat, lng } = e.latlng;
     //this.setState({guessCoords: e.latlng});
-    this.props.updateCoordinates(lat, lng)      
+    this.props.updateCoordinates(lat, lng)
 
     let marker = new Leaflet.marker([lat, lng], { draggable: true });
     marker.addTo(this.map);
 
-    marker.on('dragend', (e) => { 
-     // const {lat, lng} = e._latlng;
-     let lat = e.target._latlng.lat;
-     let long = e.target._latlng.lng;
-      this.props.updateCoordinates(lat, long);    
+    marker.on('dragend', (e) => {
+      // const {lat, lng} = e._latlng;
+      let lat = e.target._latlng.lat;
+      let long = e.target._latlng.lng;
+      this.props.updateCoordinates(lat, long);
       console.log("new guess coords: " + this.props.guessCoords);
     })
   };
 
-  showAnswer() {
+  showAnswer = () => {
     let answermarker = new Leaflet.marker(this.props.answerCoords, { draggable: false }).addTo(this.map);
     answermarker.bindPopup("<strong>Actual Location</strong>").addTo(this.map).openPopup();
   };
 
+  submitGuess = () => {
+    this.showAnswer();
+    //this.getScore()
+  }
 
   render() {
-    return <div ref={this.mapRef} id="guess-map" className="map" />;
+    return (
+      <div id="guess-area">
+        <button id="giveup">GIVEUP</button>
+        <div ref={this.mapRef} id="guess-map" className="map" />
+        <button id="submit-guess" onClick={this.submitGuess}>SUBMIT GUESS</button>
+      </div>
+    );
   }
 }
 
@@ -137,41 +175,50 @@ class Game extends React.Component {
       score: this.score || 0
     }
   }
+
   updateCoordinates = (lat, long) => {
-      this.setState({guessCoords: [lat, long]})
-      console.log('guess: ' + this.props.guessCoords)
+    this.setState({ guessCoords: [lat, long] })
+    console.log('guess: ' + this.props.guessCoords)
   }
-  /*
-  getDistance(){
+
+  getDistance = () => {
     let distance = milesPerDegree * Math.sqrt(Math.square(this.guessCoords.lat - this.answerCoords.lat) + Math.square(this.guessCoords.long - this.answerCoords.long));
     console.log('distance: ' + distance);
     return distance;
   }
-  */
 
-  getScore() {
+
+  getScore = () => {
     let distance = this.getDistance();
-    let score = Math.floor((lengthOfVT*lengthOfVT)/(distance*distance));
+    let score = Math.floor((lengthOfVT * lengthOfVT) / (distance * distance));
     console.log('score: ' + score)
-    //document.getElementById('highscores').innerHTML = "Score: " + score;
+    document.getElementById('score').innerHTML = "Score: " + score;
     return score;
   }
+
+
 
   render() {
     console.log('answer coords: ' + this.state.answerCoords)
     return (
-    <div id='grid'>
-    <div id='nav'></div>
-    <div id="game-area"><GameMap answerCoords={this.state.answerCoords} /></div>
-    <div id="guess-area"><GuessMap answerCoords={this.state.answerCoords} guessCoords={this.state.guessCoords} updateCoordinates={this.updateCoordinates}/></div>
-    </div>
+      <div id='grid'>
+        <div id='nav'>
+          <div id="score">Score</div>
+          <button id="return">RETURN TO CENTER</button>
+          <button id="highscores">SHOW HIGH SCORES</button>
+        </div>
+        <GameMap answerCoords={this.state.answerCoords} />
+        <GuessMap answerCoords={this.state.answerCoords} 
+        guessCoords={this.state.guessCoords} 
+        updateCoordinates={this.updateCoordinates} 
+        getDistance={this.getDistance}
+        getScore={this.getScore}/>
+      </div>
     )
   }
 }
 
 const pageContainer = document.getElementById("page-container");
-//ReactDOM.render(<GuessMap />, document.getElementById('guess-area'));
-//ReactDOM.render(<GameMap />, document.getElementById('game-area'));
 ReactDOM.render(<Game />, pageContainer);
 
 
